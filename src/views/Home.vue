ecd1568399ebeda5e0cdc16789b1f1d2c7687466
Oussama Sassour <oussamasassour24@gmail.com>
2026-04-02 15:48:24 +0100

    Revise Hero component details in Home.vue
    Updated Hero component title and description for clarity.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@ const currentYear = new Date().getFullYear()
     <Navbar />
     <Hero
       name="Oussama Sassour"
-      title="Full-Stack Developer · CTO Early-Stage · Coach Bootcamp"
+      title="Développeur Full-Stack · Expérience en startup · Mentor technique"
       description="Je conçois des solutions web et SaaS performantes, en alliant architecture scalable, vision produit et expérience utilisateur.
 En parallèle, je forme et accompagne des développeurs en bootcamp vers l’excellence technique."
     />
